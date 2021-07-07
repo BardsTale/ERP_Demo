a6d086314a2e5613f40af8289f8821fb0afbd3bc
BardsTale <civu@nate.com>
2021-07-07 21:02:57 +0900

    exception 주석 문구 수정
    상세 내용 : 엑셀 업로드로 문구 수정

--- TestReport/src/main/java/com/TestReport/configuration/exception/ExcelUploadException.java
+++ TestReport/src/main/java/com/TestReport/configuration/exception/ExcelUploadException.java
@@ -3,7 +3,7 @@ package com.TestReport.configuration.exception;
 public class ExcelUploadException extends RuntimeException {
 	
 	/**
-	 * 성적서 기준치 순번 중복 커스텀 예외 처리.
+	 * 엑셀 업로드 커스텀 예외 처리.
 	 * serialVersionUID Default로 넣음
 	 */
 	private static final long serialVersionUID = 2L;
